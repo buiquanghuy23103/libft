287e255a25c710819be7f6a643387f752147bd1b
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-10-31 10:00:32 +0200

    Add ft_putchar_fd and ft_putstr_fd

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -1,29 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/30 18:57:25 by hbui              #+#    #+#             */
-/*   Updated: 2021/10/31 09:57:13 by hbui             ###   ########.fr       */
+/*   Created: 2021/10/31 09:44:34 by hbui              #+#    #+#             */
+/*   Updated: 2021/10/31 09:48:47 by hbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <unistd.h>
-# include <stdlib.h>
-
-void	ft_putchar(char c);
-void	ft_putstr(char *str);
-int		ft_strlen(char *str);
-char	*ft_strcpy(char *dest, char *src);
-char	*ft_strdup(char *src);
-void	ft_putnbr(int nb);
-int		ft_strcmp(char *s1, char *s2);
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char const *s, int fd);
-
-#endif
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -1,29 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/30 18:57:25 by hbui              #+#    #+#             */
-/*   Updated: 2021/10/31 09:57:13 by hbui             ###   ########.fr       */
+/*   Created: 2021/10/31 09:49:13 by hbui              #+#    #+#             */
+/*   Updated: 2021/10/31 09:56:44 by hbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <unistd.h>
-# include <stdlib.h>
+void	ft_putstr_fd(char const *s, int fd)
+{
+	int	i;
 
-void	ft_putchar(char c);
-void	ft_putstr(char *str);
-int		ft_strlen(char *str);
-char	*ft_strcpy(char *dest, char *src);
-char	*ft_strdup(char *src);
-void	ft_putnbr(int nb);
-int		ft_strcmp(char *s1, char *s2);
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char const *s, int fd);
-
-#endif
+	i = 0;
+	while (s[i])
+	{
+		ft_putchar_fd(s[i], fd);
+		i++;
+	}
+}
