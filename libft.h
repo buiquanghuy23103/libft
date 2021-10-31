4e035c67b72167c05bb6418bf4aafe2f4f4fa366
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-10-31 12:50:50 +0200

    Fix typo in libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 18:57:25 by hbui              #+#    #+#             */
-/*   Updated: 2021/10/31 12:44:40 by hbui             ###   ########.fr       */
+/*   Updated: 2021/10/31 12:50:18 by hbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
-char	*ft_stpcpy(char *dst, const char *src);
+char	*ft_strcpy(char * dst, const char * src);
 char	*ft_strncpy(char *dst, const char *src, size_t len);
 char	*ft_strcat(char *s1, const char *s2);
 char	*ft_strncat(char *s1, const char *s2, size_t n);
