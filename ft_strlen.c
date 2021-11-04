33174f6250a5502a0a1e5207e4cc3696940ce040
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-11-04 21:21:58 +0200

    Fix return type of ft_strlen

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,23 +6,25 @@
 /*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 16:47:07 by hbui              #+#    #+#             */
-/*   Updated: 2021/11/04 19:44:42 by hbui             ###   ########.fr       */
+/*   Updated: 2021/11/04 21:20:54 by hbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *str)
+#include "libft.h"
+
+size_t	ft_strlen(const char *s)
 {
 	int	length;
 
 	length = 0;
-	if (str == 0)
+	if (s == 0)
 		return (0);
-	if (*str == 0)
+	if (*s == 0)
 		return (0);
-	while (*str)
+	while (*s)
 	{
 		length++;
-		str++;
+		s++;
 	}
 	return (length);
 }
