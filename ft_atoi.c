82a5c15a35ea05c6b04a5d1136727a0789e7b173
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-11-11 20:44:54 +0200

    Refactor ft_atoi with ft_isdigit

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 08:33:26 by hbui              #+#    #+#             */
-/*   Updated: 2021/11/11 19:57:37 by hbui             ###   ########.fr       */
+/*   Updated: 2021/11/11 20:44:02 by hbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,6 @@ int	ft_isspace(char c)
 {
 	return (c == ' ' || c == '\t' || c == '\v' || c == '\n' || c == '\f'
 		|| c == '\r');
-}
-
-int	ft_isnum(char c)
-{
-	return (c >= '0' && c <= '9');
 }
 
 int	ft_atoi(const char *str)
@@ -42,7 +37,7 @@ int	ft_atoi(const char *str)
 			sign = -1;
 		i++;
 	}
-	while (str[i] && ft_isnum(str[i]))
+	while (str[i] && ft_isdigit(str[i]))
 	{
 		num = num * 10 + str[i] - '0';
 		i++;
