0bb1e29e69de186e3514a92364e55bccbd66b793
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-11-22 17:36:38 +0200

    done memcpy

---
+++ ft_memcpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/22 16:42:38 by hbui              #+#    #+#             */
+/*   Updated: 2021/11/22 16:49:39 by hbui             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	char	*str_dst;
+	char	*str_src;
+	size_t	i;
+
+	str_dst = (char *) dst;
+	str_src = (char *) src;
+	while (i < n)
+	{
+		str_dst[i] = str_src[i];
+		i++;
+	}
+	return (dst);
+}
