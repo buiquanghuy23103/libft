f771c01308d6037342b6ad7d41d65fd0df4a12c1
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-11-22 15:38:17 +0200

    done memset

---
+++ ft_memset.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/22 14:22:48 by hbui              #+#    #+#             */
+/*   Updated: 2021/11/22 15:37:57 by hbui             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	ch;
+	char			*str;
+	int				i;
+
+	if (len <= 0)
+		return (b);
+	ch = c;
+	i = 0;
+	str = (char *) b;
+	while (i < len)
+	{
+		str[i] = c;
+		i++;
+	}
+	return (str);
+}
