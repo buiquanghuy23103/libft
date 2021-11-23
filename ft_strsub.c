d5589341eb40eea80baa9b7e5adb0071f0fa1453
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-11-23 22:08:35 +0200

    done strsub

---
+++ ft_strsub.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/23 21:24:59 by hbui              #+#    #+#             */
+/*   Updated: 2021/11/23 21:46:14 by hbui             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	int		i;
+
+	i = 0;
+	sub = ft_strnew(len);
+	if (!sub)
+		return (NULL);
+	while (i < len)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	return (sub);
+}
