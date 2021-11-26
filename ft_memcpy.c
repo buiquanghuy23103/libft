4ca9b2d7a60dc44588ccb75f648d35fcb8c2982f
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-11-26 16:25:28 +0200

    Fix memcpy: not init counter

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 16:42:38 by hbui              #+#    #+#             */
-/*   Updated: 2021/11/22 16:49:39 by hbui             ###   ########.fr       */
+/*   Updated: 2021/11/26 16:24:15 by hbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 
 	str_dst = (char *) dst;
 	str_src = (char *) src;
+	i = 0;
 	while (i < n)
 	{
 		str_dst[i] = str_src[i];
