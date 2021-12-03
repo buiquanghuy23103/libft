f8861294e51a5b63bc2537899ff513ac3d180ffb
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-12-03 16:53:11 +0200

    done strstr

---
+++ ft_strstr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/03 16:32:37 by hbui              #+#    #+#             */
+/*   Updated: 2021/12/03 16:51:43 by hbui             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strstr(const char *haystack, const char *needle)
+{
+	int	haystack_len;
+	int	needle_len;
+	int	i;
+
+	haystack_len = ft_strlen(haystack);
+	needle_len = ft_strlen(needle);
+	if (needle_len == 0)
+		return ((char *) haystack);
+	if (needle_len < haystack_len)
+	{
+		while (haystack[i])
+		{
+			if (haystack[i] == needle[0]
+				&& ft_strncmp(haystack + i, needle, needle_len) == 0)
+				return ((char *) haystack + i);
+			i++;
+		}
+	}
+	return (NULL);
+}
