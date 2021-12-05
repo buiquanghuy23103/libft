452b02732f37aaa9db287740ae62d695ede1db92
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-12-05 12:09:04 +0200

    Set pointer to null at the end of lstdelone

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 10:51:06 by hbui              #+#    #+#             */
-/*   Updated: 2021/12/05 10:56:35 by hbui             ###   ########.fr       */
+/*   Updated: 2021/12/05 11:10:12 by hbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 		del((*alst)->content, (*alst)->content_size);
 		free(*alst);
 		*alst = NULL;
+		alst = NULL;
 	}
 }
