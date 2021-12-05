d82935ba9f959c343d23fccdce937eaa27bd9734
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-12-05 15:59:16 +0200

    memcpy: handle NULL as both params with size

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 16:42:38 by hbui              #+#    #+#             */
-/*   Updated: 2021/11/26 16:24:15 by hbui             ###   ########.fr       */
+/*   Updated: 2021/12/05 15:56:07 by hbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	char	*str_src;
 	size_t	i;
 
+	if (!dst || !src)
+		return (NULL);
 	str_dst = (char *) dst;
 	str_src = (char *) src;
 	i = 0;
