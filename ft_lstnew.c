a9d600aa7fa1f17af60a459085b3b8f4745a1a83
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-12-05 11:10:53 +0200

    Set next pointer to NULL in lstnew

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 10:03:28 by hbui              #+#    #+#             */
-/*   Updated: 2021/12/05 10:25:21 by hbui             ###   ########.fr       */
+/*   Updated: 2021/12/05 11:03:55 by hbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,15 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 	t_list	*list;
 
 	list = (t_list *)ft_memalloc(sizeof(t_list));
-	if (!list)
-		return (NULL);
-	list->content = (void *)content;
-	if (!content)
-		list->content_size = 0;
-	else
-		list->content_size = content_size;
-	return (list);
+	if (list)
+	{
+		list->next = NULL;
+		list->content = (void *)content;
+		if (!content)
+			list->content_size = 0;
+		else
+			list->content_size = content_size;
+		return (list);
+	}
+	return (NULL);
 }
