b49cc4ad8f1b2e3a74f038e4a2db4b6480d615d7
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-12-05 22:20:56 +0200

    strtrim: handle empty string

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/27 11:27:45 by hbui              #+#    #+#             */
-/*   Updated: 2021/12/03 18:34:56 by hbui             ###   ########.fr       */
+/*   Updated: 2021/12/05 21:51:41 by hbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,18 @@ char	*ft_strtrim(char const *s)
 	int	start;
 	int	end;
 
+	if (!s)
+		return (NULL);
 	start = 0;
 	end = ft_strlen(s) - 1;
 	while (s[start] && ft_iswhitespace(s[start]))
 		start++;
-	while (s[end] && ft_iswhitespace(s[end]))
-		end--;
-	if (start > end)
+	if (end >= 0)
+	{
+		while (s[end] && ft_iswhitespace(s[end]))
+			end--;
+	}
+	if (start >= end)
 		return (ft_strdup(""));
 	return (ft_strsub(s, start, end - start + 1));
 }
