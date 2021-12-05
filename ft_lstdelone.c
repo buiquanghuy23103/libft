4f679ba4698ac41903aecb98066a3e76f277c117
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-12-05 10:57:17 +0200

    done lstdelone

---
+++ ft_lstdelone.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/05 10:51:06 by hbui              #+#    #+#             */
+/*   Updated: 2021/12/05 10:56:35 by hbui             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
+{
+	if (alst && *alst)
+	{
+		del((*alst)->content, (*alst)->content_size);
+		free(*alst);
+		*alst = NULL;
+	}
+}
