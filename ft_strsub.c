4b9bb8c33e1073d25b54a27396dd08558f90119d
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-12-06 22:52:49 +0200

    strsub: protect against null param

--- ft_strsub.c
+++ ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 21:24:59 by hbui              #+#    #+#             */
-/*   Updated: 2021/11/23 21:46:14 by hbui             ###   ########.fr       */
+/*   Updated: 2021/12/06 22:48:29 by hbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,17 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 	int		i;
 
 	i = 0;
-	sub = ft_strnew(len);
-	if (!sub)
-		return (NULL);
-	while (i < len)
+	if (s)
 	{
-		sub[i] = s[start + i];
-		i++;
+		sub = ft_strnew(len);
+		if (!sub)
+			return (NULL);
+		while (i < len)
+		{
+			sub[i] = s[start + i];
+			i++;
+		}
+		return (sub);
 	}
-	return (sub);
+	return (NULL);
 }
