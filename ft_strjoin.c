f394a27e2074825896bfdbaee86e23af1f99798b
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-12-06 16:44:12 +0200

    strjoin: protect from null params

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 22:19:27 by hbui              #+#    #+#             */
-/*   Updated: 2021/11/23 22:28:54 by hbui             ###   ########.fr       */
+/*   Updated: 2021/12/06 16:43:00 by hbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,26 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*join;
 
 	i = 0;
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	join = ft_strnew(s1_len + s2_len);
-	if (!join)
-		return (NULL);
-	join[s1_len + s2_len] = '\0';
-	while (i < s1_len)
+	if (s1 && s2)
 	{
-		join[i] = s1[i];
-		i++;
+		s1_len = ft_strlen(s1);
+		s2_len = ft_strlen(s2);
+		join = ft_strnew(s1_len + s2_len);
+		if (!join)
+			return (NULL);
+		join[s1_len + s2_len] = '\0';
+		while (i < s1_len)
+		{
+			join[i] = s1[i];
+			i++;
+		}
+		i = 0;
+		while (i < s2_len)
+		{
+			join[s1_len + i] = s2[i];
+			i++;
+		}
+		return (join);
 	}
-	i = 0;
-	while (i < s2_len)
-	{
-		join[s1_len + i] = s2[i];
-		i++;
-	}
-	return (join);
+	return (NULL);
 }
