feaf73c76cd96dfd8613ae08bd855bf0ee2d51e8
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-12-06 16:40:22 +0200

    strncat: allocate just enough byte

--- ft_strncat.c
+++ ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 14:18:05 by hbui              #+#    #+#             */
-/*   Updated: 2021/12/02 16:24:30 by hbui             ###   ########.fr       */
+/*   Updated: 2021/12/05 22:52:36 by hbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,17 @@
 
 char	*ft_strncat(char *s1, const char *s2, size_t n)
 {
-	int		s1_len;
+	int	s1_len;
+	int	s2_len;
+	int	k;
 
 	s1_len = ft_strlen(s1);
-	ft_memmove(s1 + s1_len, s2, n);
-	s1[s1_len + n] = '\0';
+	s2_len = ft_strlen(s2);
+	if (n >= s2_len)
+		k = s2_len;
+	else
+		k = n;
+	ft_memmove(s1 + s1_len, s2, k);
+	s1[s1_len + k] = '\0';
 	return (s1);
 }
