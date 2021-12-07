a950a7a38559a1b4361c52b22970472685c1b982
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-12-07 08:47:32 +0200

    strsplit: protect from null param

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/27 13:38:06 by hbui              #+#    #+#             */
-/*   Updated: 2021/12/04 14:40:24 by hbui             ###   ########.fr       */
+/*   Updated: 2021/12/07 08:46:59 by hbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,21 +34,12 @@ static int	transform(char *s, char c)
 	return (count);
 }
 
-char	**ft_strsplit(char const *s, char c)
+static void	ft_solve(char **split, char *dup, int str_len)
 {
-	int		i;
-	char	*dup;
-	int		str_len;
-	char	**split;
+	int	i;
+	int	c;
 
 	i = 0;
-	dup = ft_strdup(s);
-	if (!dup)
-		return (NULL);
-	str_len = ft_strlen(s);
-	split = (char **)ft_memalloc(sizeof(char *) * (transform(dup, c) + 1));
-	if (!split)
-		return (NULL);
 	c = 0;
 	while (i < str_len)
 	{
@@ -59,6 +50,24 @@ char	**ft_strsplit(char const *s, char c)
 		while (i < str_len && dup[i])
 			i++;
 	}
+}
+
+char	**ft_strsplit(char const *s, char c)
+{
+	int		i;
+	char	*dup;
+	char	**split;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	dup = ft_strdup(s);
+	if (!dup)
+		return (NULL);
+	split = (char **)ft_memalloc(sizeof(char *) * (transform(dup, c) + 1));
+	if (!split)
+		return (NULL);
+	ft_solve(split, dup, ft_strlen(s));
 	free(dup);
 	return (split);
 }
