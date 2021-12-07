a04c9db7e6173d3ddf09ce063a6877d694010f6f
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-12-07 11:09:05 +0200

    strcat: utilise strncat

--- ft_strcat.c
+++ ft_strcat.c
@@ -6,7 +6,7 @@
 /*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 14:18:05 by hbui              #+#    #+#             */
-/*   Updated: 2021/12/02 16:29:55 by hbui             ###   ########.fr       */
+/*   Updated: 2021/12/07 11:08:34 by hbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,5 @@
 
 char	*ft_strcat(char *s1, const char *s2)
 {
-	int	s1_len;
-	int	s2_len;
-
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	ft_memmove(s1 + s1_len, s2, s2_len);
-	s1[s1_len + s2_len] = '\0';
-	return (s1);
+	return (ft_strncat(s1, s2, ft_strlen(s2)));
 }
