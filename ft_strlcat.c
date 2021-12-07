3c6a392449328feba78b8a9ff060cba35771de9f
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-12-07 11:20:30 +0200

    strlcat: utilise strncat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 14:18:05 by hbui              #+#    #+#             */
-/*   Updated: 2021/12/02 22:02:08 by hbui             ###   ########.fr       */
+/*   Updated: 2021/12/07 11:20:08 by hbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	src_strlen = ft_strlen(src);
 	if (dstsize > dst_strlen)
 	{
-		while (i < dstsize - dst_strlen - 1 && src[i])
-		{
-			dst[dst_strlen + i] = src[i];
-			i++;
-		}
-		dst[dst_strlen + i] = '\0';
+		ft_strncat(dst, src, dstsize - dst_strlen - 1);
 		return (dst_strlen + src_strlen);
 	}
 	return (dstsize + src_strlen);
