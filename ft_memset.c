c83a2eda77776a728130f9a130fded1638f6685d
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-12-07 11:00:43 +0200

    memset: clean code

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 14:22:48 by hbui              #+#    #+#             */
-/*   Updated: 2021/11/22 15:38:45 by hbui             ###   ########.fr       */
+/*   Updated: 2021/12/07 10:59:44 by hbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,19 @@
 void	*ft_memset(void *b, int c, size_t len)
 {
 	unsigned char	ch;
-	char			*str;
+	unsigned char	*str;
 	int				i;
 
-	if (len <= 0)
-		return (b);
-	ch = c;
-	i = 0;
-	str = (char *) b;
-	while (i < len)
+	if (len > 0)
 	{
-		str[i] = c;
-		i++;
+		ch = (unsigned char) c;
+		i = 0;
+		str = (unsigned char *) b;
+		while (i < len)
+		{
+			str[i] = c;
+			i++;
+		}
 	}
 	return (b);
 }
