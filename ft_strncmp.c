a3cdd1c109fb1f2c07a20617f4faf6b0253fc291
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-12-07 10:09:36 +0200

    strncmp: cast 2 str to unsigned char str

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 23:42:18 by hbui              #+#    #+#             */
-/*   Updated: 2021/11/27 08:55:25 by hbui             ###   ########.fr       */
+/*   Updated: 2021/12/07 10:06:25 by hbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	int				i;
-	unsigned char	c1;
-	unsigned char	c2;
+	unsigned char	*u1;
+	unsigned char	*u2;
 
 	i = 0;
 	if (n == 0)
 		return (0);
-	while (i < n - 1 && s1[i] && s2[i] && s1[i] == s2[i])
+	u1 = (unsigned char *) s1;
+	u2 = (unsigned char *) s2;
+	while (i < n - 1 && u1[i] && u2[i] && u1[i] == u2[i])
 	{
 		i++;
 	}
-	c1 = (unsigned char) s1[i];
-	c2 = (unsigned char) s2[i];
-	return (c1 - c2);
+	return (u1[i] - u2[i]);
 }
