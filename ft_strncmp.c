9dcb26e9d3f29457a014f7e153547f68910960ff
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-12-08 20:35:43 +0200

    strncmp: cast const to const, cleaner code

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 23:42:18 by hbui              #+#    #+#             */
-/*   Updated: 2021/12/07 22:28:00 by hbui             ###   ########.fr       */
+/*   Updated: 2021/12/08 20:32:14 by hbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,16 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t			i;
-	unsigned char	*u1;
-	unsigned char	*u2;
+	size_t				i;
+	const unsigned char	*u1;
+	const unsigned char	*u2;
 
 	i = 0;
 	if (n == 0)
 		return (0);
-	u1 = (unsigned char *) s1;
-	u2 = (unsigned char *) s2;
-	while (i < n - 1 && u1[i] && u2[i] && u1[i] == u2[i])
-	{
+	u1 = (const unsigned char *) s1;
+	u2 = (const unsigned char *) s2;
+	while (i < n - 1 && u1[i] && u1[i] == u2[i])
 		i++;
-	}
 	return (u1[i] - u2[i]);
 }
