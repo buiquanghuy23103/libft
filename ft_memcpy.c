0111d91149310cb4106e5749fd2668c464f0292a
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-12-08 20:27:32 +0200

    memcpy: cast const properly

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 16:42:38 by hbui              #+#    #+#             */
-/*   Updated: 2021/12/07 10:48:50 by hbui             ###   ########.fr       */
+/*   Updated: 2021/12/08 20:26:48 by hbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	unsigned char	*str_dst;
-	unsigned char	*str_src;
-	size_t			i;
+	unsigned char		*str_dst;
+	const unsigned char	*str_src;
+	size_t				i;
 
 	if (!dst && !src)
 		return (NULL);
 	str_dst = (unsigned char *) dst;
-	str_src = (unsigned char *) src;
+	str_src = (const unsigned char *) src;
 	i = 0;
 	while (i < n)
 	{
