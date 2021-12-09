3ca8b9cea63c7adcf300490cb461717be38acca7
buiquanghuy23103 <huythdnbkltv@gmail.com>
2021-12-09 12:47:17 +0200

    memchr: cast const to const

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 18:02:03 by hbui              #+#    #+#             */
-/*   Updated: 2021/12/07 22:25:05 by hbui             ###   ########.fr       */
+/*   Updated: 2021/12/09 12:46:34 by hbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t			i;
-	unsigned char	*str_s;
-	unsigned char	unsigned_c;
+	size_t				i;
+	const unsigned char	*str_s;
+	unsigned char		unsigned_c;
 
 	i = 0;
-	str_s = (unsigned char *) s;
+	str_s = (const unsigned char *) s;
 	unsigned_c = (unsigned char) c;
 	while (i < n)
 	{
 		if (str_s[i] == unsigned_c)
-			return (str_s + i);
+			return ((void *)(str_s + i));
 		i++;
 	}
 	return (NULL);
