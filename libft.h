956ae02779efcabc1aad64bea9e4720f5f938193
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-03-31 22:43:28 +0300

    rename: ft_lst_pop to ft_lst_pop_front

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hbui <hbui@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 18:57:25 by hbui              #+#    #+#             */
-/*   Updated: 2022/03/31 22:30:30 by hbui             ###   ########.fr       */
+/*   Updated: 2022/03/31 22:41:56 by hbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,7 +92,7 @@ t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 t_list	*ft_lst_push_back(t_list **lst, void *content, size_t content_size);
 size_t	ft_lst_len(t_list *lst);
 t_list	*ft_lst_get(t_list *lst, int index);
-t_list	*ft_lst_pop(t_list **lst);
+t_list	*ft_lst_pop_front(t_list **lst);
 
 int		ft_numlen(int n);
 int		ft_abs(int n);
